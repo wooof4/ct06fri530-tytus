147f34a96910085dca891af6bb0ebd92385bc7af
wooof4 <tytus8392@gmail.com>
2025-07-18 10:46:38 +0000

    Jul 18, 2025, 10:46 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -25,7 +25,7 @@ function draw() {
   //circle(775,300,50)
   //rect(580,325,100,150)
   //triangle(1025,225,250,225,625,0)
-  fill(white)
+  fill('white')
   fill('#e3d0bf')
   rect(600,400,100,200)
   quad(600,400,550,300,750,300,700,400)
