24bcc889f160eb4dc7cd8265891e6644870c4f56
wooof4 <tytus8392@gmail.com>
2025-07-18 10:19:13 +0000

    Jul 18, 2025, 10:19 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -19,7 +19,8 @@ function draw() {
   // circle(725,250,100)fill('red')ellipse(625,425,300,100)
   // Challenge 3: A house using rect() function
   fill('red')
-  rect(425,225,500,250)
+  rect(400,225,500,250)
+  
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
