3c8fa2042d19ec354906e1b4d491d59904f187cf
wooof4 <tytus8392@gmail.com>
2025-07-18 10:30:08 +0000

    Jul 18, 2025, 10:30 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -24,7 +24,7 @@ function draw() {
   circle(500,300,50)
   circle(775,300,50)
   rect(590,325,100,150)
-  triangle(800,225,200,225,400,825)
+  triangle(800,225,200,525,400,0)
   // Challenge 4: A rocketship using triangle() function
 
   // Challenge 5: A square face using quad() function
