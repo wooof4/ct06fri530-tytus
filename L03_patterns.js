557b0cc1f0847153e9cbc647ef823136134bdadd
wooof4 <tytus8392@gmail.com>
2025-07-25 10:17:20 +0000

    Jul 25, 2025, 10:17 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -10,9 +10,7 @@ function draw() {
 let x=100;
 for(let i = 0;i < 31; i++){
   fill("red")
-  circle(100*i,300,150)
-  circle(100*i,500,150)
-  circle(100*i,400,150)
+quad()
 }
   // Task 1: Colour Gradient
 
