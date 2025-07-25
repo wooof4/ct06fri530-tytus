527a4f5639d83e5aee5299c06457a07c1956a5ac
wooof4 <tytus8392@gmail.com>
2025-07-25 10:47:16 +0000

    Jul 25, 2025, 10:47 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -7,14 +7,14 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-for(let i = 0;i <256; i++){
-  fill(255-i,0+i,0)
-circle(10*i,400,50)
-}
-for(let a = 0;a <256; a++){
-  fill(0,255-a,0+a)
-circle(2550+10*a,400,50)
-}
+//for(let i = 0;i <256; i++){
+  //fill(255-i,0+i,0)
+//circle(10*i,400,50)
+//}
+//for(let a = 0;a <256; a++){
+  //fill(0,255-a,0+a)
+//circle(2550+10*a,400,50)
+//}
 for(let b = 0;b <256; b++){
   fill(0+b,0,255-b)
 circle(5100+10*b,400,50)
