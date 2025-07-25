042bcd87195aa379e8505f33300cd38989842ba8
wooof4 <tytus8392@gmail.com>
2025-07-25 10:40:50 +0000

    Jul 25, 2025, 10:40 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -15,7 +15,10 @@ for(let a = 0;a <256; a++){
   fill(0,255-a,0+a)
 circle(2550+10*a,400,50)
 }
-
+for(let b = 0;b <256; b++){
+  fill(255-i,0+i,0)
+circle(10*i,400,50)
+}
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
