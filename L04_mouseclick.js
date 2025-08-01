08e2646a0172b0367f76bfffd08577977307c587
wooof4 <tytus8392@gmail.com>
2025-08-01 09:38:19 +0000

    Aug 1, 2025, 9:38 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,4 +1,12 @@
 // write your codes here
+function setup() {
+    createCanvas(1500, 650);
+    background(220);
+    noLoop(); // Stops continuous drawing
+  }
+  
+function draw() {
+// Recap 1: Repeating Circles
 for(let i = 0;i <256; i++){
 fill(255-i,0+i,0)
 circle(10*i,400,50)
@@ -10,4 +18,5 @@ circle(2550+10*a,400,50)
 for(let b = 0;b <256; b++){
   fill(0+b,0,255-b)
 circle(5100+10*b,400,50)
+}
 }
