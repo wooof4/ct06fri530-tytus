3745518e92a8153fd2f384bb5326831a36d3147d
wooof4 <tytus8392@gmail.com>
2025-08-01 09:36:22 +0000

    Aug 1, 2025, 9:36 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,24 +6,24 @@ function setup() {
   }
   
 function draw() {
-  // Recap 1: Repeating Circles
-//for(let i = 0;i <256; i++){
-  //fill(255-i,0+i,0)
-//circle(10*i,400,50)
-//}
-//for(let a = 0;a <256; a++){
-  //fill(0,255-a,0+a)
-//circle(2550+10*a,400,50)
-//}
-//for(let b = 0;b <256; b++){
-//  fill(0+b,0,255-b)
-//circle(5100+10*b,400,50)
-//}
-let numsquares = 9
-let count = 0;
-let boxwidth = width / numsquares
-for (let i =0;i<numsquares;i++){
-for (let j=0;j<numsquares; j++){
+// Recap 1: Repeating Circles
+for(let i = 0;i <256; i++){
+fill(255-i,0+i,0)
+circle(10*i,400,50)
+}
+for(let a = 0;a <256; a++){
+fill(0,255-a,0+a)
+circle(2550+10*a,400,50)
+}
+for(let b = 0;b <256; b++){
+  fill(0+b,0,255-b)
+circle(5100+10*b,400,50)
+}
+//let numsquares = 9
+//let count = 0;
+//let boxwidth = width / numsquares
+//for (let i =0;i<numsquares;i++){
+//for (let j=0;j<numsquares; j++){
   if (count%2==0){
     fill(0)
   }
@@ -32,8 +32,8 @@ for (let j=0;j<numsquares; j++){
   }
   count+=1
   React(i*boxwidth,j*boxwidth,boxwidth,boxwidth)
-}
-}
+
+
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
