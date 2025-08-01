eecd9ce18ed14d3a57ba8f21e354c9bfa89052a4
wooof4 <tytus8392@gmail.com>
2025-08-01 09:39:42 +0000

    Aug 1, 2025, 9:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -14,9 +14,9 @@ function draw() {
 //for(let a = 0;a <256; a++){
 //fill(0,255-a,0+a)
 //circle(2550+10*a,400,50)
-}
-for(let b = 0;b <256; b++){
-  fill(0+b,0,255-b)
-circle(5100+10*b,400,50)
-}
+//}
+//for(let b = 0;b <256; b++){
+ // fill(0+b,0,255-b)
+//circle(5100+10*b,400,50)
+//}
 }
