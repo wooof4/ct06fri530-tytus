f14c420126472dd7c7d5347c11de01b3e5fd12f7
wooof4 <tytus8392@gmail.com>
2025-08-08 10:05:39 +0000

    Aug 8, 2025, 10:05 AM

--- L04_paintbrush.js
+++ L04_paintbrush.js
@@ -4,7 +4,7 @@ let diameter = 0;
 let s = 10;
 // write your codes here
 function setup() {
-    createCanvas(1500,650);
+    createCanvas(1200,650);
     background(220);
    // noLoop(); // Stops continuous drawing
   }
