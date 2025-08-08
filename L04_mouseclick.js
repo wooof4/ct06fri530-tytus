eb9b009e93480e1c0784d26aa7777f88013dbe0c
wooof4 <tytus8392@gmail.com>
2025-08-08 09:41:49 +0000

    Aug 8, 2025, 9:41 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -59,6 +59,6 @@ function mouseDragged(){
 // circle(mouseX+random(50),mouseY+random(50),random(25))
 // fill(random(255),random(255),random(255))
 // circle(mouseX+random(50),mouseY+random(50),random(25))
-fill
+fill(shapecolor)
 circle(mouseX,mouseY,10)
 }
