beedc4fee5f54243d95ffd8c8d439743d245c11d
wooof4 <tytus8392@gmail.com>
2025-08-08 09:49:53 +0000

    Aug 8, 2025, 9:49 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -9,31 +9,7 @@ function setup() {
    // noLoop(); // Stops continuous drawing
   }
   
-function draw() {
-//background('light blue')
-// Recap 1: Repeating Circles
-//for(let i = 0;i <256; i++){
-//fill(255-i,0+i,0)
-//circle(10*i,400,50)
-//}
-//for(let a = 0;a <256; a++){
-//fill(0,255-a,0+a)
-//circle(2550+10*a,400,50)
-//}
-//for(let b = 0;b <256; b++){
- // fill(0+b,0,255-b)
-//circle(5100+10*b,400,50)
-//}
-//for(let i =0;i<256; i++){
-//fill(0+i,0+i,0+i)
-//circle(0+i*10,0+i*10,50)
-//}
-//fill(shapecolor)
-//circle(width/2,height/2,diameter)
-//textSize(28);
-//textAlign(CENTER,CENTER);
-//text('score='+score,width/2,100);
-}
+
 
 
 
