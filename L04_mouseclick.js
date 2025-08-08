aab2d53deee7c903ee27dd1a07de01a7da835cc0
wooof4 <tytus8392@gmail.com>
2025-08-08 09:44:49 +0000

    Aug 8, 2025, 9:44 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -47,7 +47,7 @@ function mouseMoved(){
 //    fill('black')
 //        circle(width/2,height/2,0+mouseY)
 //            diameter = mouseY+mouseX;
-let p=10;
+let =10;
 }
 
 function mouseDragged(){
