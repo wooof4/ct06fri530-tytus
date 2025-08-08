bac0f065f7f36e7e2b516a092d6c7c19f9bcbd92
wooof4 <tytus8392@gmail.com>
2025-08-08 10:18:08 +0000

    Aug 8, 2025, 10:18 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,7 +1,7 @@
 // write your codes here
 let moveY=0;
 function setup() {
-    createCanvas(1280,);
+    createCanvas(1280,640);
     background(220);
    // noLoop(); // Stops continuous drawing
   }
