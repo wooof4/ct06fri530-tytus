b40c8f12e031d4fb0cd391504a45106ded5cf841
wooof4 <tytus8392@gmail.com>
2025-08-08 10:57:50 +0000

    Aug 8, 2025, 10:57 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -15,16 +15,16 @@ function draw(){
     rect(moveX,moveY,10,10)
 
     if (keyIsDown(83)){
-        moveY=moveY+10;
+        moveY=moveY+100;
     }
     else if (keyIsDown(87)){
-        moveY=moveY-10;
+        moveY=moveY-100;
     }
     else if (keyIsDown(68)){
-        moveX=moveX+10;
+        moveX=moveX+100;
     }
     else if (keyIsDown(65)){
-        moveX=moveX-10;
+        moveX=moveX-100;
     }
 }
 
