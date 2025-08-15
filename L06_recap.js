1f002258743e0753eeb8312974e70f827c8c5838
wooof4 <tytus8392@gmail.com>
2025-08-15 09:51:35 +0000

    Aug 15, 2025, 9:51 AM

--- L06_recap.js
+++ L06_recap.js
@@ -13,16 +13,16 @@ function draw(){
     
     rect(moveX,moveY,10,10)
 
-    if (keyIsDown('w')){
-        moveY=moveY+10;
+    if (keyIsDown(83)){
+        moveY=moveY+100;
     }
-    else if (keyIsDown('s')){
-        moveY=moveY-10;
+    else if (keyIsDown(87)){
+        moveY=moveY-100;
     }
-    else if (keyIsDown('a')){
-        moveX=moveX+10;
+    else if (keyIsDown(68)){
+        moveX=moveX+100;
     }
-    else if (keyIsDown('d')){
-        moveX=moveX-10;
+    else if (keyIsDown(65)){
+        moveX=moveX-100;
     }
 }
