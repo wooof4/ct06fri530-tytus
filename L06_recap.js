e63e100e89343993d04edc216e0ea3b60e6205e2
wooof4 <tytus8392@gmail.com>
2025-08-15 09:51:49 +0000

    Aug 15, 2025, 9:51 AM

--- L06_recap.js
+++ L06_recap.js
@@ -6,7 +6,7 @@ function setup() {
    // noLoop(); // Stops continuous drawing
   }
 function draw(){
-    //background('light blue')
+  background('light blue')
     //noStroke()
     fill(0,0,0)
     // rect(width/2+moveX,height/2+moveY,10,10)
@@ -14,15 +14,15 @@ function draw(){
     rect(moveX,moveY,10,10)
 
     if (keyIsDown(83)){
-        moveY=moveY+100;
+        moveY=moveY+10;
     }
     else if (keyIsDown(87)){
-        moveY=moveY-100;
+        moveY=moveY-10;
     }
     else if (keyIsDown(68)){
-        moveX=moveX+100;
+        moveX=moveX+10;
     }
     else if (keyIsDown(65)){
-        moveX=moveX-100;
+        moveX=moveX-10;
     }
 }
