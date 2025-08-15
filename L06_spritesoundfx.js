a0023221e793061e5349e9d33ba1983058e9befd
wooof4 <tytus8392@gmail.com>
2025-08-15 09:43:45 +0000

    Aug 15, 2025, 9:43 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,18 +1,1 @@
 // write your codes here
- function setup() {
-    createCanvas(1200,600);
- }
-
-
-    if (keyIsDown(83)){
-        moveY=moveY+100;
-    }
-    else if (keyIsDown(87)){
-        moveY=moveY-100;
-    }
-    else if (keyIsDown(68)){
-        moveX=moveX+100;
-    }
-    else if (keyIsDown(65)){
-        moveX=moveX-100;
-    }
