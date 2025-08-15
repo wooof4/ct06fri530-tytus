64f32405ddc2a59d3a6b42709b27739960d21f3c
wooof4 <tytus8392@gmail.com>
2025-08-15 10:20:45 +0000

    Aug 15, 2025, 10:20 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,7 +1,7 @@
 // write your codes here
 let picoimg;
 function preload(){
-    picoimg=loadImage('assets/pico.png')
+    picoimg=loadImage('assets/pico.png');
 }
 function setup(){
     createCanvas(1200,700)
