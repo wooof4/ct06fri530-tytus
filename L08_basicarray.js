2e3a58c850990a2fcdc5e6eecc514ee6ad5ef04d
wooof4 <tytus8392@gmail.com>
2025-09-12 10:16:06 +0000

    Sep 12, 2025, 10:16 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,9 +1,10 @@
 let x=300;
 function setup(){
     createCanvas(600,400);
-    background(220)
-    textSize(32)
+    background(220);
+    textSize(32);
 }
 function draw(){
     text('DVD',x,y);
+    
 }
