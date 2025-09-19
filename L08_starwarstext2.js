f5e2c8eecd1027d27a8f648cf5b7dec1836833d1
wooof4 <tytus8392@gmail.com>
2025-09-19 09:42:42 +0000

    Sep 19, 2025, 9:42 AM

--- L08_starwarstext2.js
+++ L08_starwarstext2.js
@@ -1,6 +1,21 @@
-
+let storyText = [
+    "A long time ago in a galaxy far,",
+    "far away...",
+    "",
+    "EPISODE I",
+    "THE BEGINNING",
+    "",
+    "It is a period of learning...",
+    "Students have begun their journey",
+    "into the world of p5.js.",
+    "With newfound powers, they",
+    "create amazing visuals and",
+    "animations...",
+    "",
+    "May the Code be with you!"
+];
 
 
 function setup(){
-    vreateCanva(600,)
+    ceateCanva(600,400)
 }
