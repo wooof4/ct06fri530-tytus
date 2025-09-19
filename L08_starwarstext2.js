2eed2c50ebcc9495d5f9a14a5ecf976d5950da18
wooof4 <tytus8392@gmail.com>
2025-09-19 09:57:10 +0000

    Sep 19, 2025, 9:57 AM

--- L08_starwarstext2.js
+++ L08_starwarstext2.js
@@ -28,9 +28,9 @@ function draw(){
     textSize(16);
     textAlign(CENTER,CENTER);
     for(let i = 0; i<storyText.length;i++){
-        ymove=ypos+200
+    
         let sentence=storyText[i];
         text(sentence,300,ymove+i*20);    
     }
-    
+    ypos=ypos
 }
