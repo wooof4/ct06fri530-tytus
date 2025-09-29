c7f8e5fbf6c068e48afda845d903d06adc6ee69d
wooof4 <tytus8392@gmail.com>
2025-09-29 10:51:50 +0000

    Sep 29, 2025, 10:51 AM

--- L10_recap.js
+++ L10_recap.js
@@ -1,13 +1,13 @@
 let bg = 'gray';
 function setup(){
     createCanvas(400,400)
-    interval=setInterval(changeMB,)
+    interval=setInterval(changeMB,1000)
 }
 function draw(){
     background(bg);
     let s=59-second()
     textSize(100)
-    text(s,200,200);
+    text(s,100,200);
     
 }
 function changeMB(){
