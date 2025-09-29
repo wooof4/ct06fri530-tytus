6be4106871f7d3b00f13c54de8a46c3071408255
wooof4 <tytus8392@gmail.com>
2025-09-29 09:43:51 +0000

    Sep 29, 2025, 9:43 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,15 +1,15 @@
 
-function setup(){
-    createCanvas(600,400)
-    textAlign(CENTER,CENTER)
-}
-function draw(){
-    background(220)
-    fill(0)
-    textSize(72)
-    let h=hour();
-    let m=minute();
-    let s=second();
-    let timestring=nf(h,2)+ ':'+ nf(m,2) + ':' + nf(s,2)
-    text(timestring,300,200)
-}
+// function setup(){
+//     createCanvas(600,400)
+//     textAlign(CENTER,CENTER)
+// }
+// function draw(){
+//     background(220)
+//     fill(0)
+//     textSize(72)
+//     let h=hour();
+//     let m=minute();
+//     let s=second();
+//     let timestring=nf(h,2)+ ':'+ nf(m,2) + ':' + nf(s,2)
+//     text(timestring,300,200)
+// }
