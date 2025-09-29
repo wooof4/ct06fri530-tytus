f1ea1f2b57f72e31f34f629bd8cd57789d7c8138
wooof4 <tytus8392@gmail.com>
2025-09-29 10:42:54 +0000

    Sep 29, 2025, 10:42 AM

--- L10_recap.js
+++ L10_recap.js
@@ -2,12 +2,12 @@ let countdown=60;
 let s =second();
 let bg =220;
 function setup(){
-    createCanvas(600,400)
+    createCanvas(400,400)
     
 }
 function draw(){
     background(bg)
-    text(countdown,300,200);
+    text(countdown,200,200);
 }
 function mousePressed(){
 change=setInterval(changeMB,500);
