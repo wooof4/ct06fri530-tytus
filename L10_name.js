afc307c9737756b42faa651e71227c51b8449167
wooof4 <tytus8392@gmail.com>
2025-10-03 10:16:24 +0000

    Oct 3, 2025, 10:16 AM

--- L10_name.js
+++ L10_name.js
@@ -2,9 +2,9 @@ let inputtext;
 let usertext='user text here';
 function setup(){
     createCanvas(600,400);
-    input=createInput();
-    inputtext.position(width/2,height/2);
-    inputtext.input(user);
+    // input=createInput();
+    // inputtext.position(width/2,height/2);
+     inputtext.input(user);
 }
 function draw(){
     background(220);
