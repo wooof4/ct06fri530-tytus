390e919dd8f7eca7c60ccb43fad1aa2f60b3e4fb
wooof4 <tytus8392@gmail.com>
2025-10-03 10:21:05 +0000

    Oct 3, 2025, 10:21 AM

--- L10_name.js
+++ L10_name.js
@@ -4,13 +4,14 @@ function setup(){
     createCanvas(600,400);
     textSize(24)
     textAlign(CENTER,CENTER)
-}
-function draw(){
-    background(220);
     inputtext=createInput();
     inputtext.position(width,height);
     inputtext.input(user);
     usertext=inputtext;
+}
+function draw(){
+    background(220);
+
     text(usertext,width/2,height/2);
     }
 function user(){
