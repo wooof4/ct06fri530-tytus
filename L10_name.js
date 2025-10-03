82412e32b532ff067666630188fb786871794b7e
wooof4 <tytus8392@gmail.com>
2025-10-03 10:57:09 +0000

    Oct 3, 2025, 10:57 AM

--- L10_name.js
+++ L10_name.js
@@ -17,7 +17,7 @@ function setup(){
 
 function draw(){
     background(220);
-    text('name:',380,)
+    text('name:',380,50)
     text(usertext,width/2,height/2);
     text(nametext,width/2,height/2-24)
     text(hobbytext,width/2,height/2+24)
