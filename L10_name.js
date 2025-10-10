e338cfbc15b0e127ace864b2fd21985b63864b1c
wooof4 <tytus8392@gmail.com>
2025-10-10 10:01:08 +0000

    Oct 10, 2025, 10:01 AM

--- L10_name.js
+++ L10_name.js
@@ -28,7 +28,7 @@ function draw(){
     text(usertext,width/2,height/2);
     text(nametext,width/2,height/2-24)
     fill('white')
-    rect(200,75,50,100)
+    rect(200,95,50,100)
     // text(hobbytext,width/2,height/2+24)
     }
 
