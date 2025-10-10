96cb45265605aea4bf16b0e0b50507c26d5ef6c7
wooof4 <tytus8392@gmail.com>
2025-10-10 10:36:38 +0000

    Oct 10, 2025, 10:36 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -4,16 +4,17 @@ function setup(){
     createCanvas(400,600);
     noun=createInput();
     verb=createInput();
-    adjective=createInput()
-    adverb=createInput()
-    place=createInput()
-    noun.position(200,50)
-    verb.position(200,100)
-    adjective.position(200,150)
-    adverb.position(200,200)
-    place.posisition(200,250)
+    adjective=createInput();
+    adverb=createInput();
+    place=createInput();
+    
+    noun.position(200,50);
+    verb.position(200,100);
+    adjective.position(200,150);
+    adverb.position(200,200);
+    place.posisition(200,250);
 }
 function draw(){
-    background(220)
-    Text('noun:',100,60)
+    background(220);
+    Text('noun:',100,60);
 }
