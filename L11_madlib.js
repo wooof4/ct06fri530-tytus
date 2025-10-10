55cf5eff08bce26c9dcce7c9b42de6a569dafaea
wooof4 <tytus8392@gmail.com>
2025-10-10 10:36:53 +0000

    Oct 10, 2025, 10:36 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -2,17 +2,17 @@ let noun,verb,adjective,adverb,place
 
 function setup(){
     createCanvas(400,600);
-    noun=createInput();
-    verb=createInput();
-    adjective=createInput();
-    adverb=createInput();
+    noun = createInput();
+    verb = createInput();
+    adjective = createInput();
+    adverb = createInput();
     place=createInput();
     
     noun.position(200,50);
     verb.position(200,100);
     adjective.position(200,150);
     adverb.position(200,200);
-    place.posisition(200,250);
+    place.position(200,250);
 }
 function draw(){
     background(220);
