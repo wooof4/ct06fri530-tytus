698ac1e44a4a1df197294214deacd491ea1a1bca
wooof4 <tytus8392@gmail.com>
2025-10-17 09:55:23 +0000

    Oct 17, 2025, 9:55 AM

--- L12_practice.js
+++ L12_practice.js
@@ -3,6 +3,7 @@ favfood=['ramen','dog','cat','plate','chair','table','wall','block']
 function setup(){
     createCanvas(600,400)
     background(220)
+    textSize(32)
 }
 function draw(){
     for(let i = 0; i<8; i++){
