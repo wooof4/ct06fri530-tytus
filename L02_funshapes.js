b8d536c95fa94ef80ec3f0290a0686a007732df0
wooof4 <tytus8392@gmail.com>
2026-02-10 12:51:32 +0000

    Feb 10, 2026, 12:51 PM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -8,15 +8,9 @@ function setup(){
 
 function draw() {
 
-  //circle(500,300,50)
-  //circle(775,300,50)
-  //rect(580,325,100,150)
-  //triangle(1025,225,250,225,625,0)
-  //   // Challenge 4: A rocketship using triangle() function
+  circle(500,300,50)
+  circle(775,300,50)
+  rect(580,325,100,150)
+  triangle(1025,225,250,225,625,0)
 
-  // Challenge 5: A square face using quad() function
-
-  // Challenge 6: Add some colours to the drawings in the above challenges
-
-  // Challenge 7: Create a funny Pokemon
 }
